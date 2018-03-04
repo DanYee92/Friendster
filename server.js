ead7b6b751739e696a64894a3e067fcbae1d57b8
DanYee92 <danyee92@gmail.com>
2018-03-03 20:04:04 -0500

    latest updates

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-
-
-//set up dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-//creates express server and sets up a port
+var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
 
-//Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+var path=require("path");
+var PORT = process.env.PORT || 3000;
+
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 // Static files
 // needs to be called before the routes in order to work
 app.use(express.static('app/public'));
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
 
-//Router
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+//require the htmlRoutes.js file and pass it in the app variable to connect it to the module.exports function in htmlRoutes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-//Listening to the port that was set up
-app.listen(port, () => console.log("Listening on port %s", port));
+app.listen(PORT, function(){
+  console.log("App listening on PORT: " + PORT);
+});
